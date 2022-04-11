fe39003f8719e6e1b0ad95737be1f4a1a2de5257
fiskinafiulafida <fiskinafiul@gmail.com>
2022-04-11 23:38:45 +0700

    Login dan Registeer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticketing_app/views/LoginScreen.dart';
 import 'package:ticketing_app/views/home.dart';
 import 'package:ticketing_app/views/welcome.dart';
 
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primaryColor: Colors.white,
           visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: Welcome(),
+      home: LoginScreen(),
     );
   }
 }
